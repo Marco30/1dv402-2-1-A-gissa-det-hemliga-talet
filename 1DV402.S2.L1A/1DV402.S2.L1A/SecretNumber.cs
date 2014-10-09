ee19ee0e9f9409dcc7ca812d851b2e494b9c2c65
Marco30 <mv222fp@student.lnu.se>
2014-10-09 22:53:00 +0200

    lab2.1A grunden

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -4,9 +4,27 @@ namespace _1DV402.S2.L1A
 {
     class SecretNumber
     {
-        static void Main(string[] args)
+        
+            private int _Number;
+   
+            private int _Count;
+
+            public const int MaxNumberOfGuesses = 7;
+
+            private bool _gisa;
+
+        public SecretNumber()
         {
-            
+        }
+
+        public bool MakeGuess(int number)
+        {
+            return _gisa;
+        }
+
+        public void Initialize()
+        {
+
         }
     }
 }
