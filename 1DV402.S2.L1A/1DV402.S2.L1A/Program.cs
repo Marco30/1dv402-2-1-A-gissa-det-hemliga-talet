2fecca2b063264dd2743debfb858f36f0f8794af
Marco30 <mv222fp@student.lnu.se>
2014-10-10 12:23:05 +0200

    lab2.1 Initialaze funktionen

--- 1DV402.S2.L1A/1DV402.S2.L1A/Program.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/Program.cs
@@ -68,6 +68,8 @@ namespace _1DV402.S2.L1A
                 Console.CursorVisible = true;
                 Console.ResetColor();
             } while (continueGame);
+
+            
         }
     }
 }
--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 
+//Marco Villegas mitt program 
+
 namespace _1DV402.S2.L1A
 {
     class SecretNumber
     {
         
-            private int _Number;
-   
-            private int _Count;
+        private int _count;// i den här variabeln har man antal gissningar man gjort
+        private int _number;//här har man siffran man matat in  
+        public const int maxNumberOfGuesses = 7 ;//antal gånger man kan gissa bestäms i den här variablerna 
 
-            public const int MaxNumberOfGuesses = 7;
-
-            private bool _gisa;
+         
 
         public SecretNumber()
         {
@@ -19,11 +19,17 @@ namespace _1DV402.S2.L1A
 
         public bool MakeGuess(int number)
         {
-            return _gisa;
+            
         }
 
         public void Initialize()
         {
+            Random randomNumber = new Random();// anropar funktionen  som  slumpar fram nummer 
+
+            _number = randomNumber.Next(1, 100); // variabeln får ett slumpat heltal 
+
+
+            _count = 0; // variabeln _count får värdet 0
 
         }
     }
